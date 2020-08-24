1f99a9553a87f2131d17afc8ca56c765221df0e2
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-08-24 05:11:17 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+
+// TASK: Default
+gulp.task('default', function () {
+    console.log('Works fine!');
+});
+
+// TASK: SASS
+gulp.task('sass', function () {
+    return (
+        gulp.src('src/google-elevation.scss')
+            .pipe(plumber())
+            .pipe(sass())
+            .pipe(gulp.dest('dist/'))
+            .pipe(cssmin())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('dist/'))
+    );
+});
